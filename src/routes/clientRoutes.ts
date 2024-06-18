a1785ee1aeaed5ecd87b9b3d832a0af6c737062d
gastón_Alonso() <tonga88@live.com.ar>
2024-06-18 01:28:44 -0300

    .

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -6,7 +6,7 @@ import { validarToken } from '../services/generarJWT';
 const router = express.Router();
 router.get('/',getAllClient )
 router.post('/',createClient)
-router.get('/client/:id', validarToken,getClient )
-router.put('/client/:id',validarToken ,updateClient)
+router.get('/:id', validarToken,getClient )
+router.put('/:id',validarToken ,updateClient)
 
 export default router
